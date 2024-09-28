8aa25c87684d64243d4012b19529564450e9e401
chetankansal04 <ckansal0@gmail.com>
2024-09-29 00:36:16 +0530

    commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,64 +30,63 @@ export const Contact = () => {
     >
       <div className="flex flex-col transition-transform duration-300 hover:scale-110">
         <label
-          for="name"
-          class="block text-sm font-semibold leading-6 text-gray-900"
+          htmlFor="name"
+          className="block text-sm font-semibold leading-6 text-gray-900"
         >
           Name
         </label>
-        <div class="mt-2.5">
+        <div className="mt-2.5">
           <input
             type="text"
             name="from_name"
             id="name"
-            autocomplete="name"
-            class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            autoComplete="name"
+            className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           />
         </div>
       </div>
 
       <div className="flex flex-col transition-transform duration-300 hover:scale-110">
-        <div class="sm:col-span-2">
+        <div className="sm:col-span-2">
           <label
-            for="email"
-            class="block text-sm font-semibold leading-6 text-gray-900"
+            htmlFor="email"
+            className="block text-sm font-semibold leading-6 text-gray-900"
           >
             Email
           </label>
-          <div class="mt-2.5">
+          <div className="mt-2.5">
             <input
               type="email"
               name="from_email"
               id="email"
-              autocomplete="email"
-              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              autoComplete="email"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
           </div>
         </div>
       </div>
       <div className="flex flex-col transition-transform duration-300 hover:scale-110">
-        <div class="sm:col-span-2">
+        <div className="sm:col-span-2">
           <label
-            for="message"
-            class="block text-sm font-semibold leading-6 text-gray-900"
+            htmlFor="message"
+            className="block text-sm font-semibold leading-6 text-gray-900"
           >
             Message
           </label>
-          <div class="mt-2.5">
+          <div className="mt-2.5">
             <textarea
               name="message"
               id="message"
               rows="4"
-              class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             ></textarea>
           </div>
         </div>
       </div>
       <div className="flex flex-col transition-transform duration-300 hover:scale-110">
-        <label></label>
         <button
           type="submit"
-          class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Send
         </button>
