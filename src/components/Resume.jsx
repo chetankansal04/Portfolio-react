feb63511d0ace76521b5f390e14dd9aa1b297aba
chetankansal04 <ckansal0@gmail.com>
2024-09-29 21:48:25 +0530

    commit

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,7 +4,7 @@ const Resume = () => {
   return (
     <div className="place-self-center">
       <img
-        className="m-16 mr-10"
+        className="max-w-screen-sm max-h-screen m-16 mr-10"
         src="images/Chetan_Kansal_Resume-1.png"
         alt="Chetan Kansal Resume"
       ></img>
